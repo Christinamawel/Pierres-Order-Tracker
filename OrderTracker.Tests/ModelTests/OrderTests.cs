9830a11674afa86a43269450263994920e0aa9ce
Christinamawel <christinamawel@gmail.com>
2021-12-17 10:45:50 -0800

    add and pass first test in OrderTests.cs

---
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
